5ff139851147cacb92f019a21df2e503da2fa984
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2016-09-07 10:52:25 -0400

    Add record module and testing
    Records store information for each future node

---
+++ family_tree/records.py
@@ -0,0 +1,143 @@
+import difflib
+from family_tree.semester import Semester
+
+class Record:
+
+    badge_format = '{:04d}'
+
+    def __init__(self,
+            badge=None,
+            first_name=None,
+            preferred_name=None,
+            last_name=None,
+            big_badge=None,
+            pledge_semester=None,
+            **kwargs):
+
+        self.key = self.read_badge(badge)
+        self.name = self.read_name(first_name, preferred_name, last_name)
+        self.big_badge = self.read_big_badge(big_badge)
+        self.pledge_semester = self.read_pledge_semester(pledge_semester)
+
+    def read_badge(self, badge):
+        try:
+            return Record.badge_format.format(int(badge))
+        except ValueError:
+            raise RecordError('Unexpected badge number: "{}"'.format(badge))
+
+    def read_name(self, first_name, preferred_name, last_name):
+        if first_name and last_name:
+            return choose_name(first_name, preferred_name, last_name)
+        else:
+            raise RecordError('Missing first or last name')
+
+    def read_big_badge(self, big_badge):
+        if not big_badge:
+            return None
+        try:
+            return Record.badge_format.format(int(big_badge))
+        except ValueError:
+            raise RecordError('Unexpected big badge number: "{}"'.format(big_badge))
+
+    def read_pledge_semester(self, pledge_semester):
+        # Unfortunately, we will not know if we really needed the semester's
+        # value until later
+        if not pledge_semester:
+            return None
+        try:
+            return Semester(pledge_semester)
+        except ValueError:
+            return None
+
+
+class KnightRecord(Record):
+
+    pass
+
+
+class BrotherRecord(Record):
+
+    brother_id = 0
+
+    def read_badge(self, badge):
+        if badge:
+            raise RecordError('Unknighted brothers do not have badge numbers')
+        else:
+            BrotherRecord.brother_id += 1
+            return 'B{}'.format(BrotherRecord.brother_id - 1)
+
+    def read_name(self, first_name, preferred_name, last_name):
+        if last_name:
+            return last_name
+        else:
+            raise RecordError('Missing last name')
+
+class CandidateRecord(Record):
+
+    candidate_id = 0
+
+    def read_badge(self, badge):
+        if badge:
+            raise RecordError('Candidates do not have badge numbers')
+        else:
+            CandidateRecord.candidate_id += 1
+            return 'C{}'.format(CandidateRecord.candidate_id - 1)
+
+class ExpelledRecord(Record):
+
+    def read_name(self, first_name, preferred_name, last_name):
+        # Name is not strictly necessary for this program, but I require it
+        if first_name and last_name:
+            return 'Member Expelled'
+        else:
+            raise RecordError('Missing first or last name')
+
+class ReaffiliateRecord:
+
+    def __init__(self, *args, **kwargs):
+        self.key = None
+
+def choose_name(first_name, preferred_name, last_name, threshold=.5):
+    '''
+    Arguments
+    =========
+
+    first: First name
+    preferred: Preferred name
+    last: Last name
+    threshold: The distance threshold used to determine similarity
+
+    Returns
+    =======
+
+    "[preferred] [last]" if the `preferred` is not too similar to `last`
+    "[first] [last]" if `preferred` is too similar to `last`
+
+    Notes
+    =====
+
+    This might provide a marginally incorrect name for those who
+       a. go by something other than their first name that
+       b. is similar to their last name,
+    but otherwise it should almost always[^almost] provide something reasonable.
+
+    The whole point here is to
+       a. avoid using *only* last names on the tree, while
+       b. using the "first" names brothers actually go by, and while
+       c. avoiding using a first name that is a variant of the last name.
+
+    [^almost]: I say "almost always" because, for example, someone with the
+    last name "Richards" who goes by "Dick" will be listed incorrectly as "Dick
+    Richards" even if his other names are neither Dick nor Richard (unless the
+    tolerance threshold is made very low).
+    '''
+
+    if preferred_name and difflib.SequenceMatcher(None, preferred_name, last_name).ratio() < threshold:
+        return '{} {}'.format(preferred_name, last_name)
+    else:
+        return '{} {}'.format(first_name, last_name)
+
+class RecordError(Exception):
+
+    pass
+
---
+++ tests/test_records.py
@@ -0,0 +1,118 @@
+from nose.tools import *
+from family_tree.records import *
+from family_tree.semester import Semester
+
+def test_choose_name():
+
+    assert_equal(choose_name('Jon', 'Freaking', 'Snow'), 'Freaking Snow')
+    assert_equal(choose_name('Jon', 'Jonathan', 'Snow'), 'Jonathan Snow')
+    assert_equal(choose_name('Jon', 'Snowy', 'Snow'), 'Jon Snow')
+    assert_equal(choose_name('Jon', 'Snowball', 'Snow'), 'Jon Snow')
+
+    # An unfortunate compromise
+    assert_equal(choose_name('Samuel', 'Dick', 'Richards'), 'Dick Richards')
+
+    assert_not_equal(choose_name('Jon', 'Snow', 'Snow'), 'Snow Snow')
+
+def test_KnightRecord():
+
+    # No error
+    KnightRecord('9999', 'John', 'Johnny', 'Smith', '8888', 'Fall 1900')
+    KnightRecord('9999', 'John', '', 'Smith', '8888', 'Fall 1900')
+    KnightRecord('9999', 'John', None, 'Smith')
+
+    # Badge number padding
+    assert_equal(KnightRecord('1', 'John', None, 'Smith').key, '0001')
+    assert_equal(KnightRecord('-1', 'John', None, 'Smith').key, '-001') # Eh...
+
+    # No big brother
+    assert_equals(
+            KnightRecord('9999', 'John', 'Johnny', 'Smith', '', 'Fall 1900').big_badge,
+            None
+            )
+
+    # Unfortunately, we do not know whether the semester is needed
+    assert_equals(
+            KnightRecord('9999', 'John', 'Johnny', 'Smith', '8888', '').pledge_semester,
+            None
+            )
+
+    # No badge
+    assert_raises(RecordError,
+            KnightRecord, '', 'John', 'Johnny', 'Smith', '8888', 'Fall 1900',
+            )
+
+    # No first name
+    assert_raises(RecordError,
+            KnightRecord, '9999', '', 'Johnny', 'Smith', '8888', 'Fall 1900',
+            )
+
+    # No last name
+    assert_raises(RecordError,
+            KnightRecord, '9999', 'John', 'Johnny', '', '8888', 'Fall 1900',
+            )
+
+    # Malformed big badge
+    assert_raises(RecordError,
+            KnightRecord, '9999', 'John', 'Johnny', '', '0x86', 'Fall 1900',
+            )
+
+def test_BrotherRecord():
+
+    # No error
+    BrotherRecord(None, 'John', 'Johnny', 'Smith')
+
+    # First and preferred names are optional
+    BrotherRecord(None, '', 'Johnny', 'Smith')
+    BrotherRecord(None, '', '', 'Smith')
+
+    # Has badge
+    assert_raises(RecordError,
+            BrotherRecord, '1234', 'John', '', 'Smith'
+            )
+
+    # No last name
+    assert_raises(RecordError,
+            BrotherRecord, '1234', 'John', '', ''
+            )
+
+    # Brother ID
+    assert_equal(BrotherRecord(None, 'John', '', 'Smith').key, 'B3')
+
+def test_CandidateRecord():
+
+    # No error
+    CandidateRecord(None, 'John', 'Johnny', 'Smith')
+
+    # Has badge
+    assert_raises(RecordError,
+            CandidateRecord, '1234', 'John', '', 'Smith'
+            )
+
+    # No last name
+    assert_raises(RecordError,
+            CandidateRecord, '1234', 'John', '', ''
+            )
+
+    # Candidate ID
+    assert_equal(CandidateRecord(None, 'John', '', 'Smith').key, 'C1')
+
+def test_ExpelledRecord():
+
+    # No error
+    ExpelledRecord('1234', 'John', 'Johnny', 'Smith')
+
+    # No first name
+    assert_raises(RecordError,
+            ExpelledRecord, '9999', '', 'Johnny', 'Smith'
+            )
+
+    # No last name
+    assert_raises(RecordError,
+            ExpelledRecord, '9999', 'John', 'Johnny', ''
+            )
+
+def test_ReaffiliateRecord():
+
+    assert_equal(ReaffiliateRecord(*(), **{}).key, None)
+
