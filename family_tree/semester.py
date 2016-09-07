47ac64fcc682e5385b63d168a20eb3234cff1236
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2016-09-07 13:26:37 -0400

    Add typechecking in semester constructor

--- family_tree/semester.py
+++ family_tree/semester.py
@@ -8,7 +8,7 @@ class Semester(int):
 
         if isinstance(arg, int):
             value = arg
-        else:
+        elif isinstance(arg, str):
             match = Semester.matcher.match(arg)
             if match:
                 season = 1 if match.group(1) == 'Fall' else 0
@@ -16,8 +16,14 @@ class Semester(int):
                 value = 2 * year + season
             else:
                 raise ValueError(
-                    'Semester names must match "{}" but "{}" was received'
-                    .format(Semester.matcher.pattern, arg))
+                        'Semester names must match "{}" but "{}" was received'
+                        .format(Semester.matcher.pattern, arg)
+                        )
+        else:
+            raise TypeError(
+                    'Expected int or str but received {}'
+                    .format(type(arg))
+                    )
 
         return super(Semester, cls).__new__(cls, value)
 
