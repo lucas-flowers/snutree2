76d89aab2704684e66cb9dfa1af73ad47eacd462
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2016-09-09 01:07:03 -0400

    Further refactor tree_from_records code
    With defauldict, we can actually make the root node implicit.

--- family_tree/node.py
+++ family_tree/node.py
@@ -16,30 +16,25 @@ def tree_from_records(records):
 
     '''
 
-    # List of root nodes of the tree
-    roots = []
-
     # Dict of all nodes in the tree. Empty empty nodes are initialized with the
     # Node constructor (with default values for the its fields)
     nodes = defaultdict(Node)
 
     for key, record in records.items():
 
-        # The node corresponding to this record. If the node has not been
-        # created yet, the defaultdict will create an empty one.
+        # Add the record to the node corresponding to the record. If the node
+        # has not been created yet, the defaultdict will create an empty one.
         node = nodes[key]
         node.record = record
 
-        # If there is a parent key, add the node as a child to the record's
-        # parent (create the parent node if it doesn't exist)
-        if record.parent_key:
-            nodes[record.parent_key].children.append(node)
+        # Add the node as a child to the record's parent.
+        #
+        # Note: The defaultdict will create the root node implicitly, with the
+        # key of None (since records without parents have parent_key values of
+        # None).
+        nodes[record.parent_key].children.append(node)
 
-        # If there is no parent key, then this is a root node
-        else:
-            roots.append(node)
-
-    return Node(None, roots)
+    return nodes[None]
 
 class Node:
 
