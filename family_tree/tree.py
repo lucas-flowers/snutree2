52246c4d69254893c86f4aaa7850c33c9fbf2291
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2016-09-10 01:45:51 -0400

    Adjustments for networkx + Add tree decorations

--- family_tree/tree.py
+++ family_tree/tree.py
@@ -1,7 +1,28 @@
 import networkx as nx
 from collections import defaultdict
 
-def tree_from_records(records):
+graph_defaults = {
+        'size' : 80,
+        'ratio' : 'compress',
+        'pad' : '.5, .5',
+        'ranksep' : .3,
+        }
+
+node_defaults = {
+        'style' : 'filled',
+        'shape' : 'box',
+        'penwidth' : 2,
+        'width' : 2,
+        'height' : .45,
+        'fontname' : 'dejavu sans',
+        }
+
+edge_defaults = {
+        'arrowhead' : 'none',
+        }
+
+
+def records_to_networkx(records):
     '''
     Arguments
     =========
@@ -30,4 +51,22 @@ def tree_from_records(records):
             tree.add_edge(parent_key, key)
 
     return tree
+
+def decorate_tree(tree, records):
+
+    tree.graph['node'] = node_defaults
+    tree.graph['edge'] = edge_defaults
+    tree.graph['graph'] = graph_defaults
+
+    for key in tree:
+        tree.node[key]['label'] = records[key].label()
+
+
+def records_to_tree(records):
+
+    graph = records_to_networkx(records)
+    decorate_tree(graph, records)
+
+    return graph
+
 
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -5,16 +5,7 @@ from family_tree.reading import *
 from family_tree.tree import *
 
 
-def test_tree_from_records():
+def test_records_to_tree():
 
-    records = read('directory.csv', 'chapters.csv', 'brothers_not_knights.csv')
-    tree = tree_from_records(records)
-    a = nx.nx_pydot.to_pydot(tree)
-    a.write('out.dot', prog='dot')
-    # for node in tree:
-    #     if node.record and node.record.key == '1352':
-    #         assert_equal(
-    #             sorted([child.record.name for child in node]),
-    #             ['Aaron Magid', 'John Grezmak', 'Lucas Flowers', 'Tom Kan', 'Zach Palumbo'],
-    #             )
+    pass
 
