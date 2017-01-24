19004a18091b0d80a30ac5a22a195fb02352b592
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2017-01-23 20:59:46 -0500

    Add rudimentary CLI with click

--- family_tree/__main__.py
+++ family_tree/__main__.py
@@ -1,22 +1,31 @@
-import os, subprocess
+import os, subprocess, click
 from family_tree.tree import FamilyTree
 from family_tree.directory import read_settings
 from family_tree import csv, sql
 
-settings_path = 'settings.yaml'
-settings = read_settings(settings_path)
-directory = (sql if 'mysql' in settings else csv).retrieve_directory(settings)
-tree = FamilyTree(directory)
-dotgraph = tree.to_dot_graph()
-dotcode = dotgraph.to_dot()
+@click.command()
+@click.argument('settings_path')
+def main(settings_path):
+    '''
+    Create a big-little family tree.
+    '''
 
-folder = settings['output']['folder']
+    settings = read_settings(settings_path)
+    directory = (sql if 'mysql' in settings else csv).retrieve_directory(settings)
+    tree = FamilyTree(directory)
+    dotgraph = tree.to_dot_graph()
+    dotcode = dotgraph.to_dot()
 
-dot_filename = os.path.join(folder, settings['output']['name'] + '.dot')
-with open(dot_filename, 'w+') as dotfile:
-    dotfile.write(dotcode)
+    folder = settings['output']['folder']
 
-pdf_filename = os.path.join(folder, settings['output']['name'] + '.pdf')
-with open(dot_filename, 'r') as dotfile, open(pdf_filename, 'wb') as pdffile:
-    subprocess.run(['dot', '-Tpdf'], check=True, stdin=dotfile, stdout=pdffile)
+    dot_filename = os.path.join(folder, settings['output']['name'] + '.dot')
+    with open(dot_filename, 'w+') as dotfile:
+        dotfile.write(dotcode)
+
+    pdf_filename = os.path.join(folder, settings['output']['name'] + '.pdf')
+    with open(dot_filename, 'r') as dotfile, open(pdf_filename, 'wb') as pdffile:
+        subprocess.run(['dot', '-Tpdf'], check=True, stdin=dotfile, stdout=pdffile)
+
+if __name__ == '__main__':
+    main()
 
