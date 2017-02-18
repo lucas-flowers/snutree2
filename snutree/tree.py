cbc1b98b73f9ac3b3bd8f4dac55e87ec529daffe
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2017-02-18 01:00:32 -0500

    Fix indentation

--- snutree/tree.py
+++ snutree/tree.py
@@ -55,66 +55,66 @@ class FamilyTree:
 
     SETTINGS_VALIDATOR = Validator({
 
-    # Layout options
-    'layout' : {
-            'type' : 'dict',
-            'schema' : { flag : optional_boolean for flag in flags },
-            'default' : { flag : True for flag in flags },
-            },
-
-    # Default attributes for graphs, nodes, edges, and their subcategories
-    'graph_defaults' : dot_defaults('all'),
-    'node_defaults' : dot_defaults('all', 'semester', 'unknown', 'member'),
-    'edge_defaults' : dot_defaults('all', 'semester', 'unknown'),
-
-    # A mapping of node keys to colors
-    'family_colors' : {
-            'type' : 'dict',
-            'default' : {},
-            'keyschema' : nonempty_string,
-            'valueschema' : nonempty_string,
-            },
-
-    # Custom nodes, each with Graphviz attributes and a semester
-    'nodes' : {
-            'type' : 'dict',
-            'default' : {},
-            'keyschema' : nonempty_string,
-            'valueschema' : {
+        # Layout options
+        'layout' : {
                 'type' : 'dict',
-                'schema' : {
-                    'semester' : semester_like,
-                    'attributes' : attributes,
-                    }
-                }
-            },
+                'schema' : { flag : optional_boolean for flag in flags },
+                'default' : { flag : True for flag in flags },
+                },
 
-    # Custom edges: Each entry in the list has a list of nodes, which are used
-    # to represent a path from which to create edges (which is why there must
-    # be at least two nodes in each list). There are also edge attributes
-    # applied to all edges in the path.
-    'edges' : {
-            'type' : 'list',
-            'default' : [],
-            'schema' : {
+        # Default attributes for graphs, nodes, edges, and their subcategories
+        'graph_defaults' : dot_defaults('all'),
+        'node_defaults' : dot_defaults('all', 'semester', 'unknown', 'member'),
+        'edge_defaults' : dot_defaults('all', 'semester', 'unknown'),
+
+        # A mapping of node keys to colors
+        'family_colors' : {
                 'type' : 'dict',
-                'schema' : {
-                    'nodes' : {
-                        'type' : 'list',
-                        'required' : True,
-                        'minlength' : 2,
-                        'schema' : nonempty_string,
-                        },
-                    'attributes' : attributes,
+                'default' : {},
+                'keyschema' : nonempty_string,
+                'valueschema' : nonempty_string,
+                },
+
+        # Custom nodes, each with Graphviz attributes and a semester
+        'nodes' : {
+                'type' : 'dict',
+                'default' : {},
+                'keyschema' : nonempty_string,
+                'valueschema' : {
+                    'type' : 'dict',
+                    'schema' : {
+                        'semester' : semester_like,
+                        'attributes' : attributes,
+                        }
                     }
                 },
-            },
 
-    # Seed for the RNG, to provide consistent output
-    'seed': {
-            'type' : 'integer',
-            'default' : 71,
-            }
+        # Custom edges: Each entry in the list has a list of nodes, which are
+        # used to represent a path from which to create edges (which is why
+        # there must be at least two nodes in each list). There are also edge
+        # attributes applied to all edges in the path.
+        'edges' : {
+                'type' : 'list',
+                'default' : [],
+                'schema' : {
+                    'type' : 'dict',
+                    'schema' : {
+                        'nodes' : {
+                            'type' : 'list',
+                            'required' : True,
+                            'minlength' : 2,
+                            'schema' : nonempty_string,
+                            },
+                        'attributes' : attributes,
+                        }
+                    },
+                },
+
+        # Seed for the RNG, to provide consistent output
+        'seed': {
+                'type' : 'integer',
+                'default' : 71,
+                }
 
     })
 
