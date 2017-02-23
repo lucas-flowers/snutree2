715afb846148595739cd75d339d1da848c540968
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2017-02-23 14:26:06 -0500

    Move test_entity contents to sigmanu
    It was only testing a sigmanu function anyway.

--- tests/test_sigmanu.py
+++ tests/test_sigmanu.py
@@ -1,6 +1,20 @@
 import string
 from unittest import TestCase
-from snutree.member.sigmanu import Affiliation
+from snutree.member.sigmanu import Affiliation, combine_names
+
+class TestCombineNames(TestCase):
+
+    def test_combine_names(self):
+
+        self.assertEqual(combine_names('Jon', 'Freaking', 'Snow'), 'Freaking Snow')
+        self.assertEqual(combine_names('Jon', 'Jonathan', 'Snow'), 'Jonathan Snow')
+        self.assertEqual(combine_names('Jon', 'Snowy', 'Snow'), 'Jon Snow')
+        self.assertEqual(combine_names('Jon', 'Snowball', 'Snow'), 'Jon Snow')
+
+        # An unfortunate compromise
+        self.assertEqual(combine_names('Samuel', 'Dick', 'Richards'), 'Dick Richards')
+
+        self.assertNotEqual(combine_names('Jon', 'Snow', 'Snow'), 'Snow Snow')
 
 class TestAffiliation(TestCase):
 
