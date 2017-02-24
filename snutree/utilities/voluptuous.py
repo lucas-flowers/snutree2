842bfd3a99757a23ff1013944cc441258bb4eb86
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2017-02-24 18:13:22 -0500

    Comment on voluptuous module

--- snutree/utilities/voluptuous.py
+++ snutree/utilities/voluptuous.py
@@ -3,12 +3,19 @@ import re
 DIGITS_MATCHER = re.compile('\d+')
 
 def Digits(s):
+    '''
+    Matches a string that consists only of digits. Throws a ValueError on
+    failure to match.
+    '''
     match = DIGITS_MATCHER.match(s)
     if match:
         return match.group(0)
     raise ValueError
 
 def NonEmptyString(s):
+    '''
+    Matches a nonempty string and throws a ValueError otherwise.
+    '''
     if isinstance(s, str) and len(s) > 0:
         return s
     raise ValueError
