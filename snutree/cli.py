ddcc982e7aba80fedf7007ea7988b776e7dffe63
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2017-02-28 15:48:06 -0500

    Make click.option setup less ugly

--- snutree/cli.py
+++ snutree/cli.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from functools import wraps
 import click
 from . import gui, snutree, SnutreeError
 from .utilities import logged
@@ -31,18 +32,76 @@ def gui_main(ctx, param, value):
         gui.main()
         ctx.exit()
 
+options = [
+        ('--gui', '-g', {
+            'callback' : gui_main,
+            'is_flag' : True,
+            'expose_value' : False,
+            'is_eager' : True,
+            'help' : 'Use the GUI (ignores other options and parameters)'
+            }),
+        ('output_path', '--output', '-o', {
+            'type' : click.Path(),
+            'default' : None,
+            'help' : 'PDF or DOT file'
+            }),
+        ('log_path', '--log', '-l', {
+            'type' : click.Path(exists=False),
+            'default' : None,
+            'help' : 'Log location'
+            }),
+        ('config_paths', '--config', '-c', {
+            'type' : click.Path(exists=True),
+            'multiple' : True,
+            'help' : 'Tree configuration file'
+            }),
+        ('--member-format', '-m', {
+            'default' : 'basic',
+            'help' : 'Expected member format'
+            }),
+        ('input_format','--format', '-f', {
+            'type' : str,
+            'default' : None,
+            'help' : 'Input format for stdin'
+            }),
+        ('--seed', '-S', {
+            'default' : 0,
+            'help' : 'Use a different seed to move tree nodes around'
+            }),
+        ('--verbose', '-v', {
+            'is_flag' : True,
+            'default' : False,
+            'help' : 'Print progress'
+            }),
+        ('--debug', '-d', {
+            'is_flag' : True,
+            'default' : False,
+            'help' : 'Print debug information'
+            }),
+        ('--quiet', '-q', {
+            'is_flag' : True,
+            'default' : False,
+            'help' : "Don't print anything, including warnings"
+            }),
+        ]
+
+class collect_options:
+
+    def __init__(self, options_list):
+        self.options = options_list
+
+    def __call__(self, function):
+
+        accumulator = function
+        for option in options:
+            param_decls, attrs = option[:-1], option[-1]
+            accumulator = wraps(accumulator)(click.option(*param_decls, **attrs)(accumulator))
+
+        return accumulator
+
 @click.command()
 @click.argument('files', nargs=-1, type=click.File('r'))
-@click.option('--gui', '-g', callback=gui_main, is_flag=True, expose_value=False, is_eager=True)
-@click.option('output_path', '--output', '-o', type=click.Path(), default=None, help='PDF or DOT file')
-@click.option('log_path', '--log', '-l', type=click.Path(exists=False), default=None, help='Log location')
-@click.option('config_paths', '--config', '-c', type=click.Path(exists=True), multiple=True, help='Tree configuration file')
-@click.option('--member-format', '-m', default='basic', help='Expected member format')
-@click.option('input_format','--format', '-f', type=str, default=None, help='Input format for stdin')
-@click.option('--seed', '-S', default=0, help='Use a different seed to move tree nodes around')
-@click.option('--verbose', '-v', is_flag=True, default=False, help='Print progress')
-@click.option('--debug', '-d', is_flag=True, default=False, help='Print debug information')
-@click.option('--quiet', '-q', is_flag=True, default=False, help="Don't print anything, including warnings")
+@collect_options(options)
 @logged
 def cli(*args, **kwargs):
     return snutree.generate(*args, **kwargs)
