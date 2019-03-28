4fd20c2b5e527f8653cf66f306037842b3d3899b
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2019-03-27 20:10:16 -0400

    Add tests for Write.component_attributes
    Also rename that test file.

--- test/test_write_dot.py
+++ test/test_write_dot.py
@@ -3,6 +3,7 @@ import pytest
 
 from conftest import trim
 from snutree.write import dot
+from snutree.model.dot import Node
 from snutree.model.tree import Entity, Relationship, Cohort, FamilyTree
 
 @pytest.fixture
@@ -57,6 +58,87 @@ def tree():
         classes=set(),
         data={},
     )
+
+@pytest.mark.parametrize('component_type, classes, data, config, expected', [
+
+    # Empty component
+    (
+        'node', {}, {},
+        {},
+        {},
+    ),
+
+    # Class is not defined
+    (
+        'node', {'nonexistent'}, {},
+        {},
+        {},
+    ),
+
+    # Class is defined but not for the right component
+    (
+        'node', {'nonexistent'}, {},
+        {'edge': {'nonexistent': {'color': 'blue'}}},
+        {},
+    ),
+
+    # Class is defined but not used
+    (
+        'node', {}, {},
+        {'node': {'nonexistent': {'color': 'blue'}}},
+        {},
+    ),
+
+    # Class is defined and used
+    (
+        'node', {'class'}, {},
+        {'node': {'class': {'color': 'blue'}}},
+        {'color': 'blue'},
+    ),
+
+    # Order of attributes in the config dict is preserved
+    (
+        'node', {'class'}, {},
+        {'node': {'class': {'size': 10, 'color': 'blue'}}},
+        {'size': 10, 'color': 'blue'},
+    ),
+    (
+        'node', {'class'}, {},
+        {'node': {'class': {'color': 'blue', 'size': 10}}},
+        {'color': 'blue', 'size': 10},
+    ),
+
+    # The last class in the config takes priority when there are conflicts
+    (
+        'node', {'class_a', 'class_b'}, {},
+        {'node': {'class_a': {'color': 'blue'}, 'class_b': {'color': 'red'}}},
+        {'color': 'red'},
+    ),
+    (
+        'node', {'class_b', 'class_a'}, {},
+        {'node': {'class_b': {'color': 'red'}, 'class_a': {'color': 'blue'}}},
+        {'color': 'blue'},
+    ),
+
+    # Attribute order still reflects config order when there are conflicts
+    (
+        'node', {'class_a', 'class_b'}, {},
+        {'node': {'class_a': {'A': 'a', 'B': 'b'}, 'class_b': {'B': 2, 'A': 1}}},
+        {'A': 1, 'B': 2},
+    ),
+
+    # The 'label' field is a template whose values are filled by the data dict
+    (
+        'edge', {'class'}, {'name': 'Test'},
+        {'edge': {'class': {'label': 'Name is {name}', 'title': 'Title is {name}'}}},
+        {'label': 'Name is Test', 'title': 'Title is {name}'},
+    ),
+
+])
+def test_component_attributes(component_type, classes, data, config, expected):
+    write = dot.Write(config)
+    component = write.component_attributes(component_type, classes, data)
+    assert component == expected
 
 def test_dot_to_family_tree(tree, config):
     assert dot.write(tree, config) == trim(r'''
