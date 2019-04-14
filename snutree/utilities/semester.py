1abfb4589433aa6d40ac7573d4bbd2b7c9a3c2e8
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2019-04-14 14:20:01 -0400

    Update Semester class

--- snutree/utilities/semester.py
+++ snutree/utilities/semester.py
@@ -1,59 +1,65 @@
+
 import re
+from enum import Enum
+from functools import singledispatch, total_ordering
+
+@total_ordering
+class Season(Enum):
+
+    SPRING = 'Spring'
+    FALL = 'Fall'
+
+    def __str__(self):
+        return self.value
+
+    def __le__(self, other):
+        return self == self.SPRING
+
+@singledispatch
+def _semester(value: int):
+    return value
+
+@_semester.register
+def _(season: Season, year: int):
+    return 2 * year + {Season.SPRING: 0, Season.FALL: 1}[season]
+
+@_semester.register
+def _(string: str):
+    match = Semester.PATTERN_SEMESTER.match(string)
+    return _semester( # pylint: disable=too-many-function-args
+        getattr(Season, match.group('season').upper()),
+        int(match.group('year')),
+    )
 
 class Semester(int):
-    '''
-    Represents a semester such as "Fall 1950" or "Spring 2015". Integer values
-    can be added or subtracted from semesters, which increments/decrements them
-    by one semester per unit.
-    '''
 
-    matcher = re.compile(r'(Spring|Fall) (\d+)')
+    YEAR = r'\d+'
+    SEASON = fr'{Season.FALL}|{Season.SPRING}'
+    SEMESTER = fr'\s*(?P<season>{SEASON})\s*(?P<year>{YEAR})\s*'
+    PATTERN_SEMESTER = re.compile(SEMESTER, flags=re.IGNORECASE)
 
-    def __new__(cls, *arg):
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, _semester(*args, **kwargs))
 
-        if len(arg) == 1 and isinstance(arg[0], int):
-            value = arg[0]
+    @property
+    def year(self):
+        return self // 2
 
-        elif len(arg) == 1 and isinstance(arg[0], str):
-
-            arg = arg[0]
-            match = Semester.matcher.match(arg)
-            if match:
-                season = 1 if match.group(1) == 'Fall' else 0
-                year = int(match.group(2))
-                value = 2 * year + season
-            else:
-                msg = 'semester names must match "{pattern}"'.format(pattern=Semester.matcher.pattern)
-                raise ValueError(msg)
-
-        elif len(arg) == 2 and isinstance(arg[0], str) and isinstance(arg[1], int):
-
-            season, year = arg
-            season = {'Spring' : 0, 'Fall' : 1}.get(season)
-            if not season:
-                msg = 'semester seasons must match "Spring" or "Fall"'
-                raise ValueError(msg)
-
-            value = 2 * year + season
-
-        else:
-            raise TypeError('expected int, str, or *(str, int)')
-
-        return super(Semester, cls).__new__(cls, value)
+    @property
+    def season(self):
+        return Season.SPRING if self % 2 == 0 else Season.FALL
 
     def __repr__(self):
-        year, is_fall = divmod(self, 2)
-        season = 'Fall' if is_fall else 'Spring'
-        return '{season} {year}'.format(season=season, year=year)
+        return f'{self.season} {self.year}'
 
     def __str__(self):
         return repr(self)
 
     def __add__(self, other):
-        return Semester(super(Semester, self).__add__(other))
+        if isinstance(other, Semester):
+            raise TypeError('Cannot add two semesters')
+        else:
+            return type(self)(super(type(self), self).__add__(other))
 
     __radd__ = __add__
-
-    def __sub__(self, other):
-        return Semester(super(Semester, self).__sub__(other))
 
