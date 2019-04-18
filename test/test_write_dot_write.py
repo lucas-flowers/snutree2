b778d3e4a14113bac52eb2e6be2e84d140789fe8
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2019-04-17 22:29:54 -0400

    Split test_write_dot into two
    One file for testing the configuration and the other for testing the
    actual writing.

--- test/test_write_dot_write.py
+++ test/test_write_dot_write.py
@@ -2,68 +2,11 @@
 from itertools import repeat
 
 import pytest
-from jsonschema.exceptions import ValidationError
 
 from conftest import trim
 from snutree.write import dot
 from snutree.model.dot import Graph, Node, Edge
-from snutree.write.dot.config import class_map, validate
 from snutree.model.tree import Entity, Relationship, Cohort, FamilyTree
-
-@pytest.mark.parametrize('keys, error', [
-
-    ([], False),
-
-    (['root'], False),
-    (['tree'], False),
-    (['rank'], False),
-    (['custom'], False),
-
-    (['root', 'tree'], False),
-    (['root', 'rank'], False),
-    (['root', 'custom'], False),
-
-    (['tree', 'root'], True),
-    (['tree', 'rank'], False),
-    (['tree', 'custom'], False),
-
-    (['rank', 'root'], True),
-    (['rank', 'tree'], False),
-    (['rank', 'custom'], False),
-
-    (['custom', 'root'], True),
-    (['custom', 'tree'], True),
-    (['custom', 'rank'], True),
-
-    (['root', 'tree', 'rank', 'custom_a', 'custom_b'], False),
-    (['root', 'custom_a', 'tree', 'rank', 'custom_b'], True),
-
-])
-def test_class_map(keys, error):
-    mapping = {key: {} for key in keys}
-    if not error:
-        class_map(mapping)
-    else:
-        with pytest.raises(ValueError):
-            class_map(mapping)
-
-@pytest.mark.parametrize('config', [
-    {},
-    {'class': {}},
-    {'class': {'node': {}}},
-    {'class': {'node': {'custom_class': {}}}},
-    {'class': {'node': {'custom_class': {'color': 'orange'}}}},
-])
-def test_config_validate(config):
-    validate(config)
-
-@pytest.mark.parametrize('config', [
-    {'class': {'graph': {'custom_class': {}}}}, # No custom graphs
-    {'class': {'node': {'custom': {}, 'root': {}}}}, # Root must go first
-])
-def test_config_validate_invalid(config):
-    with pytest.raises(ValidationError):
-        validate(config)
 
 @pytest.mark.parametrize('component_type, classes, data, config, expected', [
 
