6dde3a3f3b0373d80f0e0f9f2190f8c5f4709c66
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2019-05-08 21:03:12 -0400

    Add deep_update function for merging dicts

--- snutree/utilities/__init__.py
+++ snutree/utilities/__init__.py
@@ -1,8 +1,34 @@
 
+from collections.abc import MutableSequence, MutableMapping
 
 def get(mapping, *args):
     value = mapping
     for arg in args:
         value = value.get(arg, {})
     return value
+
+def deep_update(*dicts):
+
+    def updated_value(old_value, new_value):
+        old_map = isinstance(old_value, MutableMapping)
+        old_seq = isinstance(old_value, MutableSequence)
+        new_map = isinstance(new_value, MutableMapping)
+        new_seq = isinstance(new_value, MutableSequence)
+        if old_map and new_map:
+            return deep_update(old_value, new_value)
+        elif old_seq and new_seq:
+            return old_value + new_value
+        elif (old_map or old_seq) and new_value is None:
+            return old_value
+        else:
+            return new_value
+
+    accumulator = {}
+    for dct in dicts:
+        accumulator.update({
+            key: updated_value(accumulator.get(key), new_value)
+            for key, new_value in (dct if dct is not None else {}).items()
+        })
+
+    return accumulator
 
---
+++ test/test_utilities_deep_update.py
@@ -0,0 +1,114 @@
+
+from types import MappingProxyType
+
+import pytest
+
+from snutree.utilities import deep_update
+
+@pytest.mark.parametrize('dicts, expected', [
+
+    # No dicts
+    [
+        (),
+        {},
+    ],
+
+    # Empty dicts
+    [
+        ({},),
+        {},
+    ],
+    [
+        ({}, {}, {}),
+        {},
+    ],
+
+    # Scalars, basic
+    [
+        ({'a': 1}, {'a': 2}, {'a': 3}),
+        {'a': 3},
+    ],
+
+    # Null overrides scalars
+    [
+        ({'a': 1}, {'a': 2}, {'a': None}),
+        {'a': None},
+    ],
+
+    # Lists, basic
+    [
+        ({'a': [1]}, {'a': [2]}, {'a': [3]}),
+        {'a': [1, 2, 3]},
+    ],
+
+    # Null does not modify lists
+    [
+        ({'a': [1]}, {'a': None}, {'a': [2]}),
+        {'a': [1, 2]},
+    ],
+
+    # Scalars override lists
+    [
+        ({'a': [1, 2, 3]}, {'a': 5}),
+        {'a': 5},
+    ],
+
+    # Strings are treated like scalars, not lists
+    [
+        ({'a': 'in'}, {'a': 'love'}, {'a': 'palisades'}),
+        {'a': 'palisades'},
+    ],
+
+    # Dicts, basic
+    [
+        ({'a': {'b': 1}}, {'a': {'b': 2}}, {'a': {'c': 3}}),
+        {'a': {'b': 2, 'c': 3}},
+    ],
+
+    # Null does not modify dicts
+    [
+        ({'a': {'b': 1}}, {'a': None}, {'a': {'c': 2}}),
+        {'a': {'b': 1, 'c': 2}},
+    ],
+
+    # Scalars override dicts
+    [
+        ({'a': {'a': 1, 'b': 2, 'c': 3}}, {'a': 5}),
+        {'a': 5},
+    ],
+
+    # Immutable maps are treated like scalars, not dicts
+    [
+        (
+            {'a': MappingProxyType({'oh': 'how'})},
+            {'a': MappingProxyType({'i': 'meant'})},
+            {'a': MappingProxyType({'no': 'harm'})},
+        ),
+        {'a': MappingProxyType({'no': 'harm'})},
+    ],
+
+    # Null-valued items in later subcollections have no effect
+    [
+        ({'a': {'b': []}}, {'a': {'b': None}}, {'a': None}, None),
+        {'a': {'b': []}},
+    ],
+
+    # But other falsy-valued items in later subcollections do have an effect
+    [
+        ({'a': {'b': []}}, {'a': {'b': ''}}, {'a': ''}),
+        {'a': ''},
+    ],
+
+    # More complex test
+    [
+        (
+            {'a': 1, 'b': 2, 'c': None, 'd': {'e': 3, 'd': 5, 'f': [1, 2, None, 3]}},
+            {'e': 5, 'd': {'e': 4, 'd': 2, 'f': [5, 2]}},
+        ),
+        {'a': 1, 'b': 2, 'c': None, 'd': {'e': 4, 'd': 2, 'f': [1, 2, None, 3, 5, 2]}, 'e': 5},
+    ],
+
+])
+def test_deep_update(dicts, expected):
+    assert deep_update(*dicts) == expected
+
