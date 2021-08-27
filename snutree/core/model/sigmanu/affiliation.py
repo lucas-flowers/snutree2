08ade69eae6420389adfa05f5bc8384729b6092a
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2021-08-27 00:24:30 -0400

    Add tests for affiliation
    Also make badge an int

--- snutree/core/model/sigmanu/affiliation.py
+++ snutree/core/model/sigmanu/affiliation.py
@@ -8,19 +8,19 @@ class Affiliation:
 
     priority: int
     designation: str
-    badge: str
+    badge: int
 
     @overload
-    def __init__(self, designation: str, badge: str, /, priority: int) -> None:
+    def __init__(self, designation: str, badge: int, /, priority: int) -> None:
         ...
 
     @overload
     def __init__(self, string: str, /, *, priority: int = ...) -> None:
         ...
 
-    def __init__(self, arg1: str, arg2: Optional[str] = None, /, priority: int = 1) -> None:
+    def __init__(self, arg1: str, arg2: Optional[int] = None, /, priority: int = 1) -> None:
 
-        if isinstance(arg2, str):
+        if arg2 is not None:
             self.designation = arg1
             self.badge = arg2
             self.priority = priority
@@ -30,7 +30,7 @@ class Affiliation:
 
             string = arg1
 
-            if not (match := self.pattern_affiliation.match(string)):
+            if not (match := self.PATTERN_AFFILIATION.match(string)):
                 raise ValueError(f"not a designation: {string}")
 
             chapter_name = match.group("chapter_name")
@@ -39,22 +39,22 @@ class Affiliation:
             # https://github.com/python/typeshed/issues/263 # For findall
             if chapter_name:
                 assert not chapter_code
-                words: list[str] = self.pattern_word.findall(chapter_name)
+                words: list[str] = self.PATTERN_WORD.findall(chapter_name)
             else:
                 assert chapter_code
-                characters: list[str] = self.pattern_character.findall(chapter_code)
-                words = [self.character_to_word[character] for character in characters]
+                characters: list[str] = self.PATTERN_CHARACTER.findall(chapter_code)
+                words = [self.CHARACTER_TO_WORD[character] for character in characters]
 
-            chapter_code = "".join(self.word_to_characters[word.title()][0] for word in words)
+            chapter_code = "".join(self.WORD_TO_CHARACTERS[word.title()][0] for word in words)
 
             self.priority = priority
             self.designation = chapter_code
-            self.badge = str(int(match.group("badge")))
+            self.badge = int(match.group("badge"))
 
     # Map of titlecase designation names to a sequence of possible,
     # capitalized, abbreviations. The first abbreviation is the primary one.
     # Note that some possible abbreviations are Latin lookalikes.
-    word_to_characters = {
+    WORD_TO_CHARACTERS = {
         "Alpha": "ΑαA",
         "Beta": "ΒβB",
         "Gamma": "Γγ",
@@ -84,25 +84,25 @@ class Affiliation:
         "(B)": ["(B)", "(b)"],
     }
 
-    character_to_word = {character: word for word, characters in word_to_characters.items() for character in characters}
+    CHARACTER_TO_WORD = {character: word for word, characters in WORD_TO_CHARACTERS.items() for character in characters}
 
-    word = r"(?i:{})".format("|".join(re.escape(word) for word in word_to_characters.keys()))
+    WORD = r"(?i:{})".format("|".join(re.escape(word) for word in WORD_TO_CHARACTERS.keys()))
 
-    character = "|".join(re.escape(character) for characters in word_to_characters.values() for character in characters)
+    CHARACTER = "|".join(re.escape(character) for characters in WORD_TO_CHARACTERS.values() for character in characters)
 
-    chapter_name = fr"({word})(\s+({word}))*"
+    CHAPTER_NAME = fr"({WORD})(\s+({WORD}))*"
 
-    chapter_code = fr"({character})+"
+    CHAPTER_CODE = fr"({CHARACTER})+"
 
-    designation = fr"(?P<chapter_name>{chapter_name})|(?P<chapter_code>{chapter_code})"
+    DESIGNATION = fr"(?P<chapter_name>{CHAPTER_NAME})|(?P<chapter_code>{CHAPTER_CODE})"
 
-    badge = "0*(?P<badge>[0-9]+)"  # Not exactly, but there's only two exceptions in all Sigma Nu
+    BADGE = "0*(?P<badge>[0-9]+)"  # Not exactly, but there's only two exceptions in all Sigma Nu
 
-    affiliation = fr"\s*({designation})\s+({badge})\s*"
+    AFFILIATION = fr"\s*({DESIGNATION})\s+({BADGE})\s*"
 
-    pattern_affiliation = re.compile(affiliation)
-    pattern_word = re.compile(word)
-    pattern_character = re.compile(character)
+    PATTERN_AFFILIATION = re.compile(AFFILIATION)
+    PATTERN_WORD = re.compile(WORD)
+    PATTERN_CHARACTER = re.compile(CHARACTER)
 
     def __str__(self) -> str:
         return f"{self.designation}\N{NO-BREAK SPACE}{self.badge}"
---
+++ tests/core/model/sigmanu/test_affiliation.py
@@ -0,0 +1,206 @@
+from contextlib import nullcontext
+from dataclasses import dataclass
+from typing import ContextManager, Optional
+
+import pytest
+
+from snutree.core.model.sigmanu.affiliation import Affiliation
+from tests.conftest import TestCase
+
+
+@dataclass
+class AffiliationTestCase(TestCase):
+    string: str
+    expected: Optional[str]
+
+
+@pytest.mark.parametrize(
+    "case",
+    [
+        # Basic abbreviated form
+        AffiliationTestCase(
+            id="basic-abbreviated",
+            string="Α 3",
+            expected="Α 3",
+        ),
+        AffiliationTestCase(
+            id="basic-abbreviated",
+            string="Π 5",
+            expected="Π 5",
+        ),
+        AffiliationTestCase(
+            id="basic-abbreviated",
+            string="ΔA 132",
+            expected="ΔΑ 132",
+        ),
+        AffiliationTestCase(
+            id="basic-abbreviated",
+            string="ABK 43925",
+            expected="ΑΒΚ 43925",
+        ),
+        AffiliationTestCase(
+            id="synonym-characters",
+            string="Σςσ 5",
+            expected="ΣΣΣ 5",
+        ),
+        AffiliationTestCase(
+            id="upper-lower-and-lookalikes",
+            string="αKΚ 43925",
+            expected="ΑΚΚ 43925",
+        ),
+        AffiliationTestCase(
+            id="upper-lower-and-lookalikes",
+            string="AαΑ(A)(a) 5",
+            expected="ΑΑΑ(A)(A) 5",
+        ),
+        AffiliationTestCase(
+            id="greek-letters",
+            string="ΗΜ 5",
+            expected="ΗΜ 5",
+        ),
+        AffiliationTestCase(
+            id="latin-letters",
+            string="HM 5",
+            expected="ΗΜ 5",
+        ),
+        AffiliationTestCase(
+            id="zeros",
+            string="Α 0",
+            expected="Α 0",
+        ),
+        AffiliationTestCase(
+            id="zeros",
+            string="Α 000000",
+            expected="Α 0",
+        ),
+        AffiliationTestCase(
+            id="zeros",
+            string="Α 0000001",
+            expected="Α 1",
+        ),
+        AffiliationTestCase(
+            id="zeros",
+            string="(A)(A) 0023",
+            expected="(A)(A) 23",
+        ),
+        AffiliationTestCase(
+            id="basic-english-name",
+            string="Alpha 5",
+            expected="Α 5",
+        ),
+        AffiliationTestCase(
+            id="basic-english-name",
+            string="Delta Alpha 5",
+            expected="ΔΑ 5",
+        ),
+        AffiliationTestCase(
+            id="basic-english-name",
+            string="(A) (B) (A) (B) 234",
+            expected="(A)(B)(A)(B) 234",
+        ),
+        AffiliationTestCase(
+            id="whitespace-in-name",
+            string="        Alpha    \t    Beta\n\n\n      5    ",
+            expected="ΑΒ 5",
+        ),
+        AffiliationTestCase(
+            id="various-cases",
+            string="sigma Sigma SIGMA sIgMa 5",
+            expected="ΣΣΣΣ 5",
+        ),
+        AffiliationTestCase(
+            id="invalid",
+            string="alfa bravo xiii",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="empty-string",
+            string="",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="needs-designation",
+            string="6",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="needs-designation",
+            string=" 6",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="no-number",
+            string="Α",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="no-number",
+            string="A ",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="no-positive-integer",
+            string="A -5",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="neither-a-greek-letter-nor-a-lookalike",
+            string="a 5",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="neither-a-greek-letter-nor-a-lookalike",
+            string="D 5",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="math-pi-product",
+            string="∏ 6",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="math-sigma-sum",
+            string="∑ 6",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="there-is-no-(C)",
+            string="ΗΜ(C) 5",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="there-is-no-(C)",
+            string="Eta Mu (C) 5",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="cannot-combine-word-form-and-character-form",
+            string="HM (A) 5",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="cannot-combine-word-form-and-character-form",
+            string="Eta Mu(B) 5",
+            expected=None,
+        ),
+        AffiliationTestCase(
+            id="cannot-combine-word-form-and-character-form",
+            string="(B)(B) (B) 5",
+            expected=None,
+        ),
+    ],
+)
+def test_affiliation_from_string(case: AffiliationTestCase) -> None:
+
+    context: ContextManager[object]
+    if case.expected is None:
+        context = pytest.raises(ValueError, match="not a designation")
+    else:
+        context = nullcontext()
+
+    with context:
+        assert str(Affiliation(case.string)) == case.expected
+
+
+def test_affiliation_direct() -> None:
+    assert str(Affiliation("Delta Alpha", 1000, priority=1)) == "Delta Alpha 1000"
