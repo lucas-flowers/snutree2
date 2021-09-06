c7c972c1da9260bee7a704ebcc7ac7216f2bc5e0
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2021-09-06 11:08:15 -0400

    Group related example files together

--- tests/approval/test_examples.py
+++ tests/approval/test_examples.py
@@ -23,17 +23,19 @@ class ExampleTestCase(TestCase):
 )
 def test_examples(pytestconfig: Config, case: ExampleTestCase) -> None:
 
-    root = Path(__file__).parent
-    input_path = (root / "examples/input" / case.name).with_suffix(".csv")
+    root = Path(__file__).parent / "examples" / case.name
+
+    input_path = root / "input.csv"
+    output_path = root / "output.dot"
+
     actual = api.run(input_path)
-    expected_path = (root / "examples/output" / case.name).with_suffix(".dot")
 
     # Do not directly assert equality, to avoid generating pytest comparison
     # output, which is really slow for the large files used in these test cases
-    if not expected_path.exists() or actual != expected_path.read_text():
+    if not output_path.exists() or actual != output_path.read_text():
         overwrite: bool = pytestconfig.getoption("--overwrite")
         if overwrite:
-            expected_path.write_text(actual)
+            output_path.write_text(actual)
             pytest.fail("output changed; overwrote sample file")
         else:
             pytest.fail("output changed")
