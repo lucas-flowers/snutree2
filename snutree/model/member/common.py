59caf6f451e3aa5b3f2a9e7b904c594f6bd20f9d
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-04-24 20:57:11 -0400

    Remove broken BaseMember stuff
    Pydantic v2 broke it.
    
    If I want to add nonempty strings back, annotated fields is probably the
    best way to do it.
    
    Also consider removing this class completely if possible.

--- snutree/model/member/common.py
+++ snutree/model/member/common.py
@@ -1,27 +1,5 @@
-from abc import ABC
-from collections.abc import Callable
-from typing import ParamSpec, TypeVar
-
-import pydantic
 from pydantic import BaseModel
 
-P_1 = ParamSpec("P_1")
-P_2 = ParamSpec("P_2")
-R = TypeVar("R")
 
-
-def identity(f: Callable[P_1, Callable[P_2, R]]) -> Callable[P_1, Callable[P_2, object]]:
-    return f
-
-
-# Remove some Any types that we don't care about
-validator = identity(pydantic.validator)
-
-
-class BaseMember(ABC, BaseModel):
-    @validator("*", pre=True)
-    def empty_strings(cls, value: object) -> object:  # pylint: disable=no-self-argument
-        if value == "":
-            return None
-        else:
-            return value
+class BaseMember(BaseModel):
+    pass
